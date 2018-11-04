391a2504412dab972fc0d7a559768144dd89e5dc
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2018-11-04 13:11:29 +0100

    Set up ninject

--- timetracker4/Program.cs
+++ timetracker4/Program.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ninject;
 using timetracker4.Forms;
 
 namespace timetracker4
 {
     static class Program
     {
+        internal static StandardKernel DependencyInjection;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            DependencyInjection = new StandardKernel();
+            DependencyInjection.Load(Assembly.GetExecutingAssembly());
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
