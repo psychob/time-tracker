718d47df423a0ce3c2883d2b37bb82e386e4381b
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2020-04-26 16:17:01 +0200

    Changes

---
+++ timetracker/StorageProvider/XML/Entity/Application.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using timetracker.Entity;
+
+namespace timetracker.StorageProvider.XML.Entity
+{
+    class Application : IApplication
+    {
+        public bool GetCountGroupAsOne()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool GetReportOnlyOne()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<IRuleSet> GetRules()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetUniqueId()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
