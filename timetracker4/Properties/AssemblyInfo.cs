7f0926162046b27e1243e526b009543e7d0aa7d1
Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
2020-05-01 14:56:46 +0200

    Add skeleton of projects inside timetracker4

---
+++ timetracker4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Time Tracker")]
+[assembly: AssemblyDescription("Application that tracks your time in applications")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RGB Lighthouse")]
+[assembly: AssemblyProduct("timetracker4")]
+[assembly: AssemblyCopyright("(c) 2015 - 2020 Andrzej Budzanowski")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e10f6f0-18fe-40bb-9876-3b17c2fdd431")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
---
+++ timetracker4/TimeTracker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using timetracker4.GUI;
+
+namespace timetracker4
+{
+    static class TimeTracker
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
